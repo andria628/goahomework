dacc9e01aa25bdfd5a1b841c4f707902f4e28e90
andria628 <androimedashvili@gmail.com>
2026-05-30 17:01:28 +0400

    second commit

---
+++ Level 30/homework/homework.py
@@ -0,0 +1,89 @@
+andria = 99
+if andria >= 10:
+    print("more than ten")
+else:
+    print("less than ten")
+
+if andria == 15:
+    print("equal to 15")
+else:
+    print("not equal 15")
+
+irakli = "group84"
+input1 = input("enter word: ")
+if irakli == input1:
+    print("you are correct")
+else:
+    print("you are not correct")
+
+for i in range(50, 100, 5):
+    print(i)
+
+name = "andria imedashvili"
+for i in name:
+    print(i)
+
+x = 20
+while x <= 50:
+    print(x)
+    x += 1
+
+for i in range(0, 100):
+    print(i)
+
+g = 0
+while g <= 100:
+    print(g)
+    g += 1
+
+for i in range(10, 20):
+    print(i)
+
+k = 10
+while k <= 20:
+    print(k)
+    k += 1
+
+for i in range(100, 200):
+    print(i)
+
+e = 100
+while e <= 200:
+    print(e)
+    e += 1
+
+irakli2 = 34
+if 34 >= 0:
+    print("correct")
+else:
+    print("uncorrect")
+    
+weekend = int(input("enter number 1-7"))
+if weekend == 1:
+    print("monday")
+elif weekend == 2:
+    print("tuesday")
+elif weekend == 3:
+    print("wednesday")
+elif weekend == 4:
+    print("thursday")
+elif weekend == 5:
+    print("friday")
+elif weekend == 6:
+    print("saturday")
+elif weekend == 7:
+    print("sunday")
+else:
+    print("ეგ არ ვიცი რა დღეა")
+
+andria5 = int(input("enter your number: "))
+if andria5 >= 50:
+    print(andria5 * 5)
+else:
+    print(andria5 * andria5)
+
+andria6 = input("password: ")
+if andria6 == "goa123":
+    print("correct password")
+else:
+    print("uncorrect password")
